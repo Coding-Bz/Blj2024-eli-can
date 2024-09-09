6ba2a49e8b7594ab31121be482ba26a3c14d3ce9
Coding-Bz <elif.canmaya@edu.tbz.ch>
2024-09-09 16:37:34 +0200

    aufgaben

---
+++ C/2024/37-01-aufgabe1/Aufgabe 2.2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Elif Canmaya\n");
+    printf("Borrweg 55\n"); printf("8055 ZH\n");
+    printf("+41 78 675 45 15\n");
+
+    return 0;
+}
---
+++ C/2024/37-01-aufgabe1/Aufgabe 3.2/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(void) {
+    printf(float f = 5.67);
+    printf("Speicherbedarf int: %d Byte (s)\n", sizeof(int));
+    return 0;
+}
