0df31cb56903da862b30d541c95d0936ef83f421
Coding-Bz <elif.canmaya@edu.tbz.ch>
2025-03-13 15:24:17 +0100

    11-coordinate_challenge
    This is the ended version of task 3. Now the whole programm works with userinput so its really easy to build any shape you want.

--- Java-Challenges/Coordinate_Challenge/src/Starter.java
+++ Java-Challenges/Coordinate_Challenge/src/Starter.java
@@ -1,21 +1,75 @@
+import java.awt.*;
+import java.util.*;
+
 public class Starter {
     public static void main(String[] args) {
-CoordinateSystem cs=new CoordinateSystem(900,900);
+        CoordinateSystem cs = new CoordinateSystem(900, 900);
+        Scanner scanner = new Scanner(System.in);
+        int numberpointers;
+        ArrayList<CSLineSegment> belief = new ArrayList<>();
+        ArrayList<Integer> hope = new ArrayList<>();
+        boolean controll = false;
+        String useranswer;
 
-        cs.addPoint(new CSPoint(200,0));
-        cs.addPoint(new CSPoint(12,40));
-
-
-        CSRenderer renderer=new CSRenderer(cs);
-
-        CSLineSegment line1=new CSLineSegment(new CSPoint(200,0),new CSPoint(12,40));
-
-        cs.addLineSegment(line1);
+        System.out.println("Welcome to this program 😀");
 
 
-        new CSRenderer(cs);
+        while (!controll) {
+            System.out.println("Please enter 'start' for the example:");
+            useranswer = scanner.next();
+            if (useranswer.equalsIgnoreCase("Start")) {
+                controll = true;
+            }
+        }
 
+      /*  System.out.println("Wich color should the lines be? 1=rot, 2=blau, 3=schwarz");
+        int Ihateusers = scanner.nextInt();
+        switch (Ihateusers) {
+            case 1:
+                @Override
 
+                        g2d.setColor(Color.RED);
+            case 2:
+                @Override
+                        g2d.setColor(Color.BLUE);
+            case 3:
+                @Override
+                        g2d.setColor(Color.BLACK);
 
+        }*/
+                do {
+                    System.out.println("How many points do you want to draw?");
+                    numberpointers = scanner.nextInt();
+                    controll = true;
+
+                    if (numberpointers == 0 || numberpointers == 1) {
+                        System.out.println("That is not possible.");
+                        System.out.println("Try again.");
+                        controll = false;
+                    }
+                } while (!controll);
+
+                for (int i = 0; i < numberpointers; i++) {
+                    String ID = "";
+                    for (int j = 0; j < 10; j++) {
+                        char first = (char) ('A' + Math.random() * ('Z' - 'A' + 1));
+                        ID += first;
+                    }
+
+                    System.out.println("Enter coordinates for point 1 (x, y):");
+                    int userchoicex = scanner.nextInt();
+                    int userchoicey = scanner.nextInt();
+
+                    System.out.println("Enter coordinates for point 2 (x, y):");
+                    int userchoicex2 = scanner.nextInt();
+                    int userchoicey2 = scanner.nextInt();
+
+                    CSLineSegment line = new CSLineSegment(new CSPoint(userchoicex, userchoicey),
+                            new CSPoint(userchoicex2, userchoicey2));
+                    belief.add(line);
+                    cs.addLineSegment(line);
+                }
+
+                CSRenderer renderer = new CSRenderer(cs);
+        }
     }
-}
